da52407aba9992afbf332536e1ed69429583fcf5
Your Name <you@example.com>
2018-04-27 10:50:08 -0400

    Merging sparse implementation

--- conv_naive.cpp
+++ conv_naive.cpp
@@ -1,9 +1,16 @@
 #include <iostream>
 #include <random>
+#include <cmath>
+
+inline bool is_nearly_equal(float x, float y)
+{
+    const float epsilon = 1e-5;
+    return std::abs(x - y) <= epsilon * std::abs(x);
+}
 
 // Indexing function into a 4-d tensor
-inline unsigned int I(unsigned int i1, unsigned int i2, unsigned int i3, unsigned int i4,
-                      unsigned int d1, unsigned int d2, unsigned int d3, unsigned int d4) {
+inline unsigned int I4(unsigned int i1, unsigned int i2, unsigned int i3, unsigned int i4,
+                       unsigned int d1, unsigned int d2, unsigned int d3, unsigned int d4) {
     return i1 * d2 * d3 * d4 + i2 * d3 * d4 + i3 * d4 + i1;
 }
 
@@ -23,7 +30,8 @@ inline unsigned int I(unsigned int i1, unsigned int i2, unsigned int i3, unsigne
 // F_in is a tensor of dimension b x (h+2p) x (w+2p) x c_in
 // F_out is a tensor of dimension b x ((h - f + 2p)/s + 1) x ((w - f + 2p)/s + 1) x c_out
 // W is a tensor of dimension c_out x c_in x f x f
-int conv_naive(float *F_in, float *W, float *F_out,
+// M is a tensor of dimension b x ((h - f + 2p)/s + 1) x ((w - f + 2p)/s + 1)
+int conv_naive(float *F_in, float *W, float *F_out, bool *M,
                unsigned int b, unsigned int h, unsigned int w,
                unsigned int c_in, unsigned int c_out,
                unsigned int f, unsigned int s, unsigned int p) {
@@ -33,16 +41,23 @@ int conv_naive(float *F_in, float *W, float *F_out,
     for (unsigned int b_i = 0; b_i < b; b_i++) {
         for (unsigned int h_i = 0; h_i < h_out; h_i++) {
             for (unsigned int w_i = 0; w_i < w_out; w_i++) {
+                // Skip computation where the mask is zero
+                if (M != nullptr) {
+                    unsigned int M_offset = I4(0, b_i, h_i, w_i, 1, b, h_out, w_out);
+                    if (!M[M_offset]) {
+                        continue;
+                    }
+                }
                 for (unsigned int c_out_i = 0; c_out_i < c_out; c_out_i++) {
-                    unsigned int out_offset = I(b_i, h_i, w_i, c_out_i,
+                    unsigned int out_offset = I4(b_i, h_i, w_i, c_out_i,
                                                 b, h_out, w_out, c_out);
                     F_out[out_offset] = 0.0f;
                     for (unsigned int c_in_i = 0; c_in_i < c_in; c_in_i++) {
-                        unsigned int in_offset = I(b_i, s*h_i + p, s*w_i + p, c_in_i,
+                        unsigned int in_offset = I4(b_i, s*h_i + p, s*w_i + p, c_in_i,
                                                    b, h, w, c_in);
                         for (unsigned int f_h = 0; f_h < f; f_h++) {
                             for (unsigned int f_w = 0; f_w < f; f_w++) {
-                                unsigned int w_offset = I(c_out_i, c_in_i, f_h, f_w,
+                                unsigned int w_offset = I4(c_out_i, c_in_i, f_h, f_w,
                                                           c_out, c_in, f, f);
                                 F_out[out_offset] += W[w_offset] * F_in[in_offset];
                             }
@@ -55,16 +70,6 @@ int conv_naive(float *F_in, float *W, float *F_out,
     return 0;
 }
 
-// F_in is a tensor of dimension b x (h+2p) x (w+2p) x c_in
-// F_out is a tensor of dimension b x ((h - f + 2p)/s + 1) x ((w - f + 2p)/s + 1) x c_out
-// W is a tensor of dimension c_out x c_in x f x f
-// M is a tensor of dimension b x ((h - f + 2p)/s + 1) x ((w - f + 2p)/s + 1) x c_out
-int sparse_conv_naive(float *F_in, float *W, float *F_out, bool *M,
-                      unsigned int b, unsigned int h, unsigned int w,
-                      unsigned int c_in, unsigned int c_out) {
-    return 0;
-}
-
 // Randomly initialize a 4d-tensor
 int init_random(float *T, unsigned int d1, unsigned int d2, unsigned int d3, unsigned int d4) {
     std::default_random_engine generator;
@@ -73,7 +78,7 @@ int init_random(float *T, unsigned int d1, unsigned int d2, unsigned int d3, uns
         for (unsigned int i2 = 0; i2 < d2; i2++) {
             for (unsigned int i3 = 0; i3 < d3; i3++) {
                 for (unsigned int i4 = 0; i4 < d4; i4++) {
-                    unsigned int offset = I(i1, i2, i3, i4, d1, d2, d3, d4);
+                    unsigned int offset = I4(i1, i2, i3, i4, d1, d2, d3, d4);
                     T[offset] =  distribution(generator);
                 }
             }
@@ -102,7 +107,7 @@ int main() {
     init_random(W, c_out, c_in, f, f);
     init_random(F_out, b, h_out, w_out, c_out);
 
-    conv_naive(F_in, W, F_out, b, h, w, c_in, c_out, f, s, p);
+    conv_naive(F_in, W, F_out, nullptr, b, h, w, c_in, c_out, f, s, p);
 
     return 0;
 }
