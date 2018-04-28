38ec384f554a75eb7d21c0dd568624a2c1d86c70
Your Name <you@example.com>
2018-04-28 11:09:47 -0400

    Adding sparsity generator

--- conv_naive.cpp
+++ conv_naive.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <random>
 #include <cmath>
+#include <cassert>
 
 inline bool is_nearly_equal(float x, float y)
 {
@@ -11,7 +12,7 @@ inline bool is_nearly_equal(float x, float y)
 // Indexing function into a 4-d tensor
 inline unsigned int I4(unsigned int i1, unsigned int i2, unsigned int i3, unsigned int i4,
                        unsigned int d1, unsigned int d2, unsigned int d3, unsigned int d4) {
-    return i1 * d2 * d3 * d4 + i2 * d3 * d4 + i3 * d4 + i1;
+    return i1 * d2 * d3 * d4 + i2 * d3 * d4 + i3 * d4 + i4;
 }
 
 // Convolution layer parameters
@@ -50,15 +51,18 @@ int conv_naive(float *F_in, float *W, float *F_out, bool *M,
                 }
                 for (unsigned int c_out_i = 0; c_out_i < c_out; c_out_i++) {
                     unsigned int out_offset = I4(b_i, h_i, w_i, c_out_i,
-                                                b, h_out, w_out, c_out);
+                                                 b, h_out, w_out, c_out);
+                    //assert(out_offset >= 0 && out_offset < (b * h_out * w_out * c_out));
                     F_out[out_offset] = 0.0f;
                     for (unsigned int c_in_i = 0; c_in_i < c_in; c_in_i++) {
                         unsigned int in_offset = I4(b_i, s*h_i + p, s*w_i + p, c_in_i,
-                                                   b, h, w, c_in);
+                                                    b, h, w, c_in);
+                        //assert(in_offset >= 0 && in_offset < (b * h * w * c_in));
                         for (unsigned int f_h = 0; f_h < f; f_h++) {
                             for (unsigned int f_w = 0; f_w < f; f_w++) {
                                 unsigned int w_offset = I4(c_out_i, c_in_i, f_h, f_w,
-                                                          c_out, c_in, f, f);
+                                                           c_out, c_in, f, f);
+                                //assert(w_offset >= 0 && w_offset < (c_out * c_in * f * f));
                                 F_out[out_offset] += W[w_offset] * F_in[in_offset];
                             }
                         }
@@ -71,7 +75,7 @@ int conv_naive(float *F_in, float *W, float *F_out, bool *M,
 }
 
 // Randomly initialize a 4d-tensor
-int init_random(float *T, unsigned int d1, unsigned int d2, unsigned int d3, unsigned int d4) {
+int generate_random_tensor(float *T, unsigned int d1, unsigned int d2, unsigned int d3, unsigned int d4) {
     std::default_random_engine generator;
     std::normal_distribution<float> distribution(0.0, 1.0);
     for (unsigned int i1 = 0; i1 < d1; i1++) {
@@ -87,10 +91,27 @@ int init_random(float *T, unsigned int d1, unsigned int d2, unsigned int d3, uns
     return 0;
 }
 
+int generate_sparsity_pattern(bool *T,  unsigned int d1, unsigned int d2, unsigned int d3,
+                              float sparsity) {
+
+    assert(sparsity >= 0 && sparsity <= 1);
+    std::default_random_engine generator;
+    std::bernoulli_distribution distribution(sparsity);
+    for (unsigned int i1 = 0; i1 < d1; i1++) {
+        for (unsigned int i2 = 0; i2 < d2; i2++) {
+            for (unsigned int i3 = 0; i3 < d3; i3++) {
+                unsigned int offset = I4(0, i1, i2, i3, 1, d1, d2, d3);
+                T[offset] =  distribution(generator);
+            }
+        }
+    }
+    return 0;
+}
+
 int main() {
     unsigned int f = 3;
     unsigned int c_in = 16;
-    unsigned int c_out = 64;
+    unsigned int c_out = 16;
     unsigned int h = 256;
     unsigned int w = 256;
     unsigned int p = 1;
@@ -102,12 +123,20 @@ int main() {
     float *F_in = new float[b * (h + 2*p) * (w + 2*p) * c_in];
     float *W = new float[c_out * c_in * f * f];
     float *F_out = new float[b * h_out * w_out * c_out];
+    bool *M = new bool[b * h_out * w_out];
 
-    init_random(F_in, b, h + 2*p, w + 2*p, c_in);
-    init_random(W, c_out, c_in, f, f);
-    init_random(F_out, b, h_out, w_out, c_out);
+    generate_random_tensor(F_in, b, h + 2*p, w + 2*p, c_in);
+    generate_random_tensor(W, c_out, c_in, f, f);
+    generate_random_tensor(F_out, b, h_out, w_out, c_out);
+    generate_sparsity_pattern(M, b, h_out, w_out, 0.1);
 
     conv_naive(F_in, W, F_out, nullptr, b, h, w, c_in, c_out, f, s, p);
+    //conv_naive(F_in, W, F_out, M, b, h, w, c_in, c_out, f, s, p);
+
+    delete F_in;
+    delete W;
+    delete F_out;
+    delete M;
 
     return 0;
 }
