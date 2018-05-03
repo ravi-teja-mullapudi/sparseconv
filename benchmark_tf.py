5b6d56ff37c5b3cad9a5f2e407c030b620365746
Prashanth Menon <pmenon@cs.cmu.edu>
2018-05-03 17:34:38 -0400

    Benchmark tensorflow

--- benchmark_tf.py
+++ benchmark_tf.py
@@ -5,13 +5,14 @@ from __future__ import print_function
 import tensorflow as tf
 import numpy as np
 import time
+import sys
 
 def run_sub_graph(out_dict, in_dict, num_runs):
     res = {}
     best_time = float('inf')
     with tf.Session() as sess:
         sess.run(tf.global_variables_initializer())
-        for t in xrange(0, num_runs):
+        for t in range(0, num_runs):
             start_time = time.time() * 1000
             res = sess.run(out_dict, feed_dict = in_dict)
             end_time = time.time() * 1000
@@ -47,14 +48,14 @@ def benchmark_conv2d(batch_size,
     _, best_time = run_sub_graph(out_dict, in_dict, num_runs)
     return best_time
 
-def main():
+def do_one_layer(layer, c_i, c_o, in_h, in_w):
     batch_size = 1
-    in_w = 256
-    in_h = 256
-    c_i = 16
+    #in_w = 256
+    #in_h = 256
+    #c_i = 16
     k_h = 3
     k_w = 3
-    c_o = 64
+    #c_o = 64
     s_h = 1
     s_w = 1
     out_w = in_w/s_w
@@ -72,8 +73,29 @@ def main():
                            device='/cpu:0')
     gops = (batch_size * out_w * out_h * c_i * c_o * k_w * k_h)/1e09
     gflops = float(gops)/(t/1e03)
-    print('Time:', t)
-    print('GFLOPS:', gflops)
+    print("Layer {}, {}, {}".format(layer, t, gflops))
+
+def main():
+    hw_arr =   [226, 226, 114, 114,  58,  58,  58,  30,  30,  30,  16,  16,  16]
+    cin_arr =  [  3,  64,  64, 128, 128, 256, 256, 256, 512, 512, 512, 512, 512]
+    cout_arr = [ 64,  64, 128, 128, 256, 256, 256, 512, 512, 512, 512, 512, 512]
+
+    layer = int(sys.argv[1])
+    cin = int(sys.argv[2])
+    cout = int(sys.argv[3])
+    h = int(sys.argv[4])
+    w = h
+
+    print("Layer: {}, C_in: {}, C_out: {}, h x w: {} x {}".format(layer, cin, cout, h, w))
+
+    do_one_layer(layer, cin, cout, h, w)
+
+    #for layer in range(len(hw_arr)):
+    #    cin = cin_arr[layer]
+    #    cout = cout_arr[layer]
+    #    h = hw_arr[layer]
+    #    w = hw_arr[layer]
+    #    do_one_layer(layer, cin, cout, h, w)
 
 if __name__ == '__main__':
     main()
---
+++ run_bench_tf.sh
@@ -0,0 +1,14 @@
+
+python benchmark_tf.py 1   3  64 226 226
+python benchmark_tf.py 2   64  64 226 226
+python benchmark_tf.py 3   64 128 114 114
+python benchmark_tf.py 4   128 128 114 114
+python benchmark_tf.py 5   128 256  58  58
+python benchmark_tf.py 6   256 256  58  58
+python benchmark_tf.py 7   256 256  58  58
+python benchmark_tf.py 8   256 512  30  30
+python benchmark_tf.py 9   512 512  30  30
+python benchmark_tf.py 10 512 512  30  30
+python benchmark_tf.py 11  512 512  16  16
+python benchmark_tf.py 12  512 512  16  16
+python benchmark_tf.py 13  512 512  16  16
